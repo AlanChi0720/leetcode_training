0686abf83a8b6911a1d5b8830f453662c5c3bb48
Alan Chi <alan860720@gmail.com>
2022-06-21 22:01:03 -0400

    'alan'

--- leetcode-09.py
+++ leetcode-09.py
@@ -1,24 +1,24 @@
-# def isPalidrome(x):
-#     if x < 0: return False
-
-#     div = 1
-#     while x >= 10 * div:
-#         div*=10
-
-#     while x:
-#         right = x % 10
-#         left = x // div
-
-#         if left != right: return False
-
-#         x = (x % div) // 10
-#         div = div / 100
-#     return True
-
 def isPalidrome(x):
-    if str(x)== str(x)[::-1]:
-        return True
-    else:
-        return False
+    if x < 0: return False
 
-print(isPalidrome(123))
+    div = 1
+    while x >= 10 * div:
+        div*=10
+
+    while x >= 0:
+        right = x % 10
+        left = x // div
+
+        if left != right: return False
+
+        x = (x % div) // 10
+        div = div / 100
+    return True
+
+# def isPalidrome(x):
+#     if str(x)== str(x)[::-1]:
+#         return True
+#     else:
+#         return False
+
+print(isPalidrome(121))
