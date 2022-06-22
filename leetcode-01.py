309d62301b365f6efbacc1edf927ae1f757b9260
Alan Chi <alan860720@gmail.com>
2022-06-21 21:30:13 -0400

    'alan'

--- leetcode-01.py
+++ leetcode-01.py
@@ -1,22 +1,21 @@
-class Solution(object):
-    def twosum(self, nums, target):
-        prevMap = {}
-        for i , n in enumerate(nums):
-            diff = target - n 
-            if diff in prevMap:
-                return [prevMap[diff], i]
-            prevMap[n]=i
-        return
+# def twosum(nums, target):
+#     prevMap = {}
+#     for i , n in enumerate(nums):
+#         diff = target - n 
+#         if diff in prevMap:
+#             return [prevMap[diff], i]
+#         prevMap[n]=i
+#     return 
 
-# class Solution(object):
-#     def twosum(self, nums, target):
-#         keymap={}
-#         for i in range(len(nums)):
-#             if target - nums[i] in keymap:
-#                 return [keymap[target-nums[i],i]]
-#             if nums[i] not in keymap:
-#                 keymap[nums[i]] = i
+
+def twosum(nums, target):
+    keymap={}
+    for i in range(len(nums)):
+        if target - nums[i] in keymap:
+            return [keymap[target-nums[i]],i]
+        if nums[i] not in keymap:
+            keymap[nums[i]] = i
 
 nums= [2,3,5,7,11,13]
 target=9
-print(Solution().twosum(nums,target))
+print(twosum(nums,target))
