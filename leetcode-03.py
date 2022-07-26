12eed1eb26805961417595629237aeb3c1d45d26
AlanChi0720 <alan860720@gmail.com>
2022-07-26 14:26:12 -0400

    'alan'

---
+++ leetcode-03.py
@@ -0,0 +1,7 @@
+class Solution:
+    def lengthOfLongestSubstring(self, s: str) -> int:
+        
+
+s = "abcabcbb"
+ans = Solution().lengthOfLongestSubstring(s)
+print(s)
--- leetcode-28.py
+++ leetcode-28.py
@@ -33,6 +33,37 @@ def strStr(haystack: str, needle: str) -> int:
             return i
     return -1
 
+def strStr(haystack: str, needle: str) -> int:
+    # KMP algorithm
+    if needle == "": return 0
+    lps = [0] * len(needle)
+    prevLPS, i = 0, 1
+    while i < len(needle):
+        if needle[i] == needle[prevLPS]:
+            lps[i] = prevLPS +1
+            prevLPS += 1
+            i += 1
+        elif prevLPS == 0:
+            lps[i] = 0
+            i += 1
+        else:
+            prevLPS = lps[prevLPS -1]
+    
+    i = 0 # for hay
+    j = 0 # for needle
+    while i < len(haystack):
+        if haystack[i] == needle[j]:
+            i += 1
+            j += 1
+        elif haystack[i] != needle[j]:
+            if j == 0:
+                i += 1
+            else:
+                j = lps[j -1]
+        if j == len(needle):
+            return i - len(needle)
+    return -1
+
 haystack = "hello"
 needle = "ll"
 # haystack = "aaaaa"
